e33fcf2a4386dff75acd4b8077f642539ced5fa9
SnowDiles <brahimboulahiapro@gmail.com>
2025-03-20 11:30:22 +0100

    [feat] add of custom cursor

--- portfolio/src/app/portfolio/page.tsx
+++ portfolio/src/app/portfolio/page.tsx
@@ -4,18 +4,47 @@ import Presentation from '@/components/presentation';
 import About from '@/components/about'
 import Projects from '@/components/projects';
 import ListIcon from '@/components/listicon';
+import AnimatedCursor from "react-animated-cursor"
 
 const PortfolioPage: React.FC = () => {
   return (
 
-    
+
     <div className='bg-[#0a1930]  h-screen' style={{ fontFamily: "'Roboto Mono', monospace" }}>
       <Navbar />
       <div>
+        <AnimatedCursor
+          innerSize={12} 
+          outerSize={35} 
+          color="152, 211, 219" 
+          outerAlpha={0.3} 
+          innerScale={0.8} 
+          outerScale={2} 
+          clickables={[
+            'a',
+            'input[type="text"]',
+            'input[type="email"]',
+            'input[type="number"]',
+            'input[type="submit"]',
+            'input[type="image"]',
+            'label[for]',
+            'select',
+            'textarea',
+            'button',
+            '.link'
+          ]}
+          trailingSpeed={7} 
+          outerStyle={{
+            border: `2px solid rgba(152, 211, 219)`, 
+          }}
+          innerStyle={{
+            backgroundColor: `rgba(152, 211, 219)`, 
+          }}
+        />
         <Presentation />
-        <About/>
-        <Projects/>
-        <ListIcon/>
+        <About />
+        <Projects />
+        <ListIcon />
       </div>
     </div>
   );
--- portfolio/src/components/projects.tsx
+++ portfolio/src/components/projects.tsx
@@ -1,7 +1,6 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { motion } from "framer-motion";
-import { Scrollbar } from 'react-scrollbars-custom';
 
 interface Repo {
   id: number;
